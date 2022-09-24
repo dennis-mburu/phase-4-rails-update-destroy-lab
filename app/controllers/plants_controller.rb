3c1635ff452fab263e0cb603f455fb96ac94269b
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-09-24 21:08:09 +0300

    Assignment Complete

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_json_error_response
 
   # GET /plants
   def index
@@ -18,9 +19,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH/PUT '/plants/:id'
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: 202
+  end
+
+  # DELETE "/plants/:id"
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head 204
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_json_error_response
+    render json: {error: "Plant not found"}, status: 404
+  end
+
 end
